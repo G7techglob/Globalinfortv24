99f4ef0e2dc0d3175c1cc86c2d00ef1d976c5f38
G7techglob <globalinfortv24@gmail.com>
2026-05-31 07:43:06 +0200

    Update entertainment.js

--- entertainment.js
+++ entertainment.js
@@ -1,5 +1,43 @@
 // MOBILE MENU
 
+const feeds = [
+  "https://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
+  "https://www.billboard.com/feed/",
+  "https://www.rollingstone.com/music/music-news/feed/",
+  "https://variety.com/feed/"
+];
+const newsGrid = document.querySelector(".news-grid");
+
+feeds.forEach(feed => {
+  fetch(API + encodeURIComponent(feed))
+    .then(res => res.json())
+    .then(data => {
+
+      data.items.slice(0, 4).forEach(item => {
+
+        const card = document.createElement("article");
+        card.className = "news-card";
+
+        card.innerHTML = `
+          <img src="${item.thumbnail || 'images/default.jpg'}" alt="">
+          <div class="card-content">
+            <span>Entertainment</span>
+            <h3>${item.title}</h3>
+            <a href="${item.link}" target="_blank">
+              <button class="card-btn">Read Story</button>
+            </a>
+          </div>
+        `;
+
+        newsGrid.appendChild(card);
+
+      });
+
+    })
+    .catch(err => console.log("RSS error:", err));
+});
+
+const API = "https://api.rss2json.com/v1/api.json?rss_url=";
 const menuBtn = document.getElementById("menu-btn");
 const navbar = document.getElementById("navbar");
 
