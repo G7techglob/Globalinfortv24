b086fdeb264b35523b69d3f0189e53161c8aede9
G7techglob <globalinfortv24@gmail.com>
2026-06-19 14:03:52 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,5 @@
-// 
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBANgzG2rW8QGdhTcd0ceL7PHwAk4bYfDg",
@@ -20,8 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-export { app, auth, db };
+export { auth };
