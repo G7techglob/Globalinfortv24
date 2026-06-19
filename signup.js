bcbc771939931324e500f35140b3049a6ac7a3b7
G7techglob <globalinfortv24@gmail.com>
2026-06-19 14:02:35 +0200

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,29 @@
+import { auth } from "./firebase.js";
+
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+const signupBtn = document.getElementById("signupBtn");
+
+signupBtn.addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    alert("Account created successfully!");
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+});
